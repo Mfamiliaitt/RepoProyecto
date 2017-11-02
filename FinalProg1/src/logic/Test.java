bfe2c39e512d3fb8296c8865d0282268fcb619a3
Felix G. Tejada <Felix G. Tejada@FELIX-PC>
2017-11-02 11:01:50 -0400

    la para

--- FinalProg1/src/logic/Test.java
+++ FinalProg1/src/logic/Test.java
@@ -1,5 +1,18 @@
 package logic;
 
 public class Test {
-
+	private String laPrueba;
+	private int laPara;
+	public String getLaPrueba() {
+		return laPrueba;
+	}
+	public void setLaPrueba(String laPrueba) {
+		this.laPrueba = laPrueba;
+	}
+	public int getLaPara() {
+		return laPara;
+	}
+	public void setLaPara(int laPara) {
+		this.laPara = laPara;
+	}
 }
