e2e308548e8e4978d152c177c3460a2d5f6ba154
vilmanicoll <vilmanicoll@VilmaNVidal-PC>
2017-11-02 11:06:28 -0400

    Hola

---
+++ FinalProg1/src/logic/Test2.java
@@ -0,0 +1,43 @@
+package logic;
+
+public class Test2 {
+
+	private String name;
+	private int cant;
+	private String codigo;
+	
+	public Test2(String name, int cant, String codigo) {
+		super();
+		this.name = name;
+		this.cant = cant;
+		this.codigo = codigo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCant() {
+		return cant;
+	}
+
+	public void setCant(int cant) {
+		this.cant = cant;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+	
+
+}
