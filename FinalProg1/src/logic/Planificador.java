feed5346a4616bff790e817d3fdc988096fa8110
vilmanicoll <vilmanicoll@VilmaNVidal-PC>
2017-11-04 17:16:15 -0400

    Cree las clases de cliente y contrato

--- FinalProg1/src/logic/Planificador.java
+++ FinalProg1/src/logic/Planificador.java
@@ -2,11 +2,13 @@ package logic;
 
 public class Planificador extends Diseniador {
 	protected int cantDias;
+	
 	public Planificador(String identificador, String nombre, String apellidos, String direccion, String sexo, int edad,
 			double salario, String nombreProyecto, String evaluacionAnual, int cantDias) {
 		super(identificador, nombre, apellidos, direccion, sexo, edad, salario, nombreProyecto, evaluacionAnual);
 		this.cantDias = cantDias;
 	}
+	
 	public int getCantDias() {
 		return cantDias;
 	}
--- FinalProg1/src/logic/Programador.java
+++ FinalProg1/src/logic/Programador.java
@@ -2,11 +2,13 @@ package logic;
 
 public class Programador extends Diseniador {
 	protected String lenguaje;
+	
 	public Programador(String identificador, String nombre, String apellidos, String direccion, String sexo, int edad,
 			double salario, String nombreProyecto, String evaluacionAnual, String lenguaje) {
 		super(identificador, nombre, apellidos, direccion, sexo, edad, salario, nombreProyecto, evaluacionAnual);
 		this.lenguaje = lenguaje;
 	}
+	
 	public String getLenguaje() {
 		return lenguaje;
 	}
