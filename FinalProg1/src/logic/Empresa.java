61b5d415f3e8e2b6e2078eaf4648832bb0c1e2aa
Manuel Familia <Manuel Familia@123cloro>
2017-11-18 11:41:16 -0400

    Sets y gets @ empresa

--- FinalProg1/src/logic/Empresa.java
+++ FinalProg1/src/logic/Empresa.java
@@ -7,6 +7,24 @@ public class Empresa {
  private ArrayList<Empleado> misEmpleados;
  private ArrayList<Contrato> misContratos;
  private ArrayList<Cliente> misClientes;
+public ArrayList<Empleado> getMisEmpleados() {
+	return misEmpleados;
+}
+public void setMisEmpleados(ArrayList<Empleado> misEmpleados) {
+	this.misEmpleados = misEmpleados;
+}
+public ArrayList<Contrato> getMisContratos() {
+	return misContratos;
+}
+public void setMisContratos(ArrayList<Contrato> misContratos) {
+	this.misContratos = misContratos;
+}
+public ArrayList<Cliente> getMisClientes() {
+	return misClientes;
+}
+public void setMisClientes(ArrayList<Cliente> misClientes) {
+	this.misClientes = misClientes;
+}
 
  
 	
