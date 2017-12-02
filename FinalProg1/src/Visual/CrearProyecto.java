59cf0758eb3524bbfa19173902bea613addf208a
vilmanicoll <vilmanicoll@VilmaNVidal-PC>
2017-12-02 15:12:20 -0400

    Aún diseniando...

--- FinalProg1/src/Visual/CrearProyecto.java
+++ FinalProg1/src/Visual/CrearProyecto.java
@@ -119,6 +119,261 @@ public class CrearProyecto extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		P2 = new JPanel();
+		P2.setBackground(Color.WHITE);
+		P2.setBounds(0, 0, 724, 440);
+		contentPanel.add(P2);
+		P2.setLayout(null);
+		P2.setVisible(false);
+		JPanel panelInfo = new JPanel();
+		panelInfo.setLayout(null);
+		panelInfo.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Informaci\u00F3n del cliente", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelInfo.setBackground(Color.WHITE);
+		panelInfo.setBounds(255, 11, 413, 239);
+		P2.add(panelInfo);
+		{
+			JLabel label = new JLabel("C\u00E9dula:");
+			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+			label.setBounds(10, 35, 46, 14);
+			panelInfo.add(label);
+		}
+		{
+			JLabel label = new JLabel("Tel\u00E9fono:");
+			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+			label.setBounds(10, 196, 63, 14);
+			panelInfo.add(label);
+		}
+		{
+			JLabel label = new JLabel("Nombre: ");
+			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+			label.setBounds(10, 84, 63, 14);
+			panelInfo.add(label);
+		}
+		{
+			JLabel label = new JLabel("Direcci\u00F3n: ");
+			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+			label.setBounds(10, 157, 63, 14);
+			panelInfo.add(label);
+		}
+		{
+			txtDireccion = new JTextField();
+			txtDireccion.setColumns(10);
+			txtDireccion.setBounds(96, 151, 292, 20);
+			panelInfo.add(txtDireccion);
+		}
+		{
+			txtNombre = new JTextField();
+			txtNombre.setColumns(10);
+			txtNombre.setBounds(96, 78, 292, 20);
+			panelInfo.add(txtNombre);
+		}
+		{
+			JLabel lblNewLabel = new JLabel("Apellido:");
+			lblNewLabel.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+			lblNewLabel.setBounds(10, 121, 76, 14);
+			panelInfo.add(lblNewLabel);
+		}
+		{
+			txtApellido = new JTextField();
+			txtApellido.setColumns(10);
+			txtApellido.setBounds(96, 115, 292, 20);
+			panelInfo.add(txtApellido);
+		}
+		
+		try {
+			MaskFormatter ced = new MaskFormatter("###-#######-#");
+			ced.setPlaceholderCharacter('_');
+			txtCedulacliente = new JFormattedTextField(ced);	
+		} catch (Exception e) {}
+		
+		txtCedulacliente.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {									
+				Cliente c=Empresa.getInstance().buscarClientePorCedula(txtCedulacliente.getText());
+				
+				if (c!=null){
+		txtCedulacliente.setText((" ")+c.getId());
+		txtNombre.setText((" ")+c.getNombre());
+		txtApellido.setText((" ")+c.getApellido());
+		txtDireccion.setText((" ")+c.getDireccion());
+		txtTelefonoclient.setText((" ")+c.getTelefono());
+		
+		txtCedulacliente.setEnabled(false);
+		txtNombre.setEnabled(false);
+		txtApellido.setEnabled(false);
+		txtDireccion.setEnabled(false);
+		txtTelefonoclient.setEnabled(false);
+		
+				}
+				else{
+				
+		txtCedulacliente.setText(" ");
+		txtNombre.setText(" ");
+		txtApellido.setText(" ");
+		txtDireccion.setText(" ");
+		txtTelefonoclient.setText(" ");
+		
+		txtCedulacliente.setEnabled(true);
+		txtNombre.setEnabled(true);
+		txtApellido.setEnabled(true);
+		txtDireccion.setEnabled(true);
+		txtTelefonoclient.setEnabled(true);
+		
+				}
+				
+			}
+		});
+		txtCedulacliente.setBounds(76, 32, 128, 20);
+		panelInfo.add(txtCedulacliente);		
+		
+		try {
+			MaskFormatter tel = new MaskFormatter("###-###-####");
+			tel.setPlaceholderCharacter('_');
+			txtTelefonoclient = new JFormattedTextField(tel);
+		} catch (Exception e) {}
+		
+		txtTelefonoclient.setBounds(96, 190, 128, 20);
+		panelInfo.add(txtTelefonoclient);
+		{
+			JLabel lblNewLabel_1 = new JLabel("");
+			lblNewLabel_1.setIcon(new ImageIcon(CrearProyecto.class.getResource("/Imagenes/avatar-grande (3).png")));
+			lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNewLabel_1.setBounds(26, 31, 217, 173);
+			P2.add(lblNewLabel_1);
+		}
+		
+	
+		
+
+	
+		{
+			P3 = new JPanel();
+			P3.setBackground(Color.WHITE);
+			P3.setBounds(0, 0, 724, 428);
+			contentPanel.add(P3);
+			P3.setLayout(null);
+			P3.setVisible(false);
+			{
+				JPanel panel = new JPanel();
+				panel.setLayout(null);
+				panel.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Informaci\u00F3n del Cliente:", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+				panel.setBackground(Color.WHITE);
+				panel.setBounds(10, 11, 704, 108);
+				P3.add(panel);
+				{
+					JLabel label = new JLabel("Nombre:");
+					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+					label.setBounds(10, 70, 57, 14);
+					panel.add(label);
+				}
+				{
+					txtNombreCmostrado = new JTextField();
+					txtNombreCmostrado.setEditable(false);
+					txtNombreCmostrado.setColumns(10);
+					txtNombreCmostrado.setBounds(77, 67, 408, 21);
+					panel.add(txtNombreCmostrado);
+				}
+				{
+					JLabel label = new JLabel("C\u00E9dula:");
+					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+					label.setBounds(10, 29, 57, 14);
+					panel.add(label);
+				}
+				{
+					txtCedulaMostrada = new JTextField();
+					txtCedulaMostrada.setEditable(false);
+					txtCedulaMostrada.setColumns(10);
+					txtCedulaMostrada.setBounds(77, 24, 117, 21);
+					panel.add(txtCedulaMostrada);
+				}
+				{
+					JLabel label = new JLabel("Tel\u00E9fono:");
+					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+					label.setBounds(204, 29, 57, 14);
+					panel.add(label);
+				}
+				{
+					txtTelMostrado = new JTextField();
+					txtTelMostrado.setEditable(false);
+					txtTelMostrado.setColumns(10);
+					txtTelMostrado.setBounds(268, 24, 217, 21);
+					panel.add(txtTelMostrado);
+				}
+			}
+			{
+				JPanel panel = new JPanel();
+				panel.setLayout(null);
+				panel.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Informaci\u00F3n Del Proyecto:", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+				panel.setBackground(Color.WHITE);
+				panel.setBounds(10, 130, 704, 108);
+				P3.add(panel);
+				{
+					JLabel label = new JLabel("Nombre:");
+					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+					label.setBounds(10, 18, 55, 14);
+					panel.add(label);
+				}
+				{
+					txtNombreProMostrado = new JTextField();
+					txtNombreProMostrado.setEditable(false);
+					txtNombreProMostrado.setColumns(10);
+					txtNombreProMostrado.setBounds(140, 15, 476, 20);
+					panel.add(txtNombreProMostrado);
+				}
+				{
+					JLabel label = new JLabel("Fecha de entrega:");
+					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+					label.setBounds(328, 50, 110, 14);
+					panel.add(label);
+				}
+				{
+					txtFechaEntregaMostrada = new JTextField();
+					txtFechaEntregaMostrada.setText((String) null);
+					txtFechaEntregaMostrada.setEditable(false);
+					txtFechaEntregaMostrada.setColumns(10);
+					txtFechaEntregaMostrada.setBounds(438, 47, 178, 20);
+					panel.add(txtFechaEntregaMostrada);
+				}
+				{
+					JLabel label = new JLabel("Total a pagar:");
+					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+					label.setBounds(10, 81, 85, 14);
+					panel.add(label);
+				}
+				{
+					txtPrecioMostrado = new JTextField();
+					txtPrecioMostrado.setEditable(false);
+					txtPrecioMostrado.setColumns(10);
+					txtPrecioMostrado.setBounds(140, 78, 178, 20);
+					panel.add(txtPrecioMostrado);
+				}
+				{
+					JLabel label = new JLabel("Fecha de Realizaci\u00F3n:");
+					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
+					label.setBounds(10, 53, 131, 14);
+					panel.add(label);
+				}
+				{
+					txtFechRealizacionMostrada = new JTextField();
+					txtFechRealizacionMostrada.setText((String) null);
+					txtFechRealizacionMostrada.setEditable(false);
+					txtFechRealizacionMostrada.setColumns(10);
+					txtFechRealizacionMostrada.setBounds(140, 50, 178, 20);
+					panel.add(txtFechRealizacionMostrada);
+				}
+			}
+			
+			JPanel panel = new JPanel();
+			panel.setLayout(null);
+			panel.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Equipo de Trabajo", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+			panel.setBackground(Color.WHITE);
+			panel.setBounds(10, 235, 704, 170);
+			P3.add(panel);
+			
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.setBounds(10, 22, 684, 137);
+			panel.add(scrollPane);
+		}
 		
 		P1 = new JPanel();
 		P1.setBackground(Color.WHITE);
@@ -322,261 +577,6 @@ public class CrearProyecto extends JDialog {
 								label.setBounds(23, 11, 219, 167);
 								P1.add(label);
 							}
-		P2 = new JPanel();
-		P2.setBackground(Color.WHITE);
-		P2.setBounds(0, 0, 724, 440);
-		contentPanel.add(P2);
-		P2.setLayout(null);
-		P2.setVisible(false);
-		JPanel panelInfo = new JPanel();
-		panelInfo.setLayout(null);
-		panelInfo.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Informaci\u00F3n del cliente", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panelInfo.setBackground(Color.WHITE);
-		panelInfo.setBounds(10, 26, 387, 239);
-		P2.add(panelInfo);
-		{
-			JLabel label = new JLabel("C\u00E9dula:");
-			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-			label.setBounds(10, 35, 46, 14);
-			panelInfo.add(label);
-		}
-		{
-			JLabel label = new JLabel("Tel\u00E9fono:");
-			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-			label.setBounds(10, 196, 63, 14);
-			panelInfo.add(label);
-		}
-		{
-			JLabel label = new JLabel("Nombre: ");
-			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-			label.setBounds(10, 84, 63, 14);
-			panelInfo.add(label);
-		}
-		{
-			JLabel label = new JLabel("Direcci\u00F3n: ");
-			label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-			label.setBounds(10, 157, 63, 14);
-			panelInfo.add(label);
-		}
-		{
-			txtDireccion = new JTextField();
-			txtDireccion.setColumns(10);
-			txtDireccion.setBounds(76, 154, 292, 20);
-			panelInfo.add(txtDireccion);
-		}
-		{
-			txtNombre = new JTextField();
-			txtNombre.setColumns(10);
-			txtNombre.setBounds(76, 81, 292, 20);
-			panelInfo.add(txtNombre);
-		}
-		{
-			JLabel lblNewLabel = new JLabel("Apellido:");
-			lblNewLabel.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-			lblNewLabel.setBounds(10, 121, 46, 14);
-			panelInfo.add(lblNewLabel);
-		}
-		{
-			txtApellido = new JTextField();
-			txtApellido.setColumns(10);
-			txtApellido.setBounds(76, 118, 292, 20);
-			panelInfo.add(txtApellido);
-		}
-		
-		try {
-			MaskFormatter ced = new MaskFormatter("###-#######-#");
-			ced.setPlaceholderCharacter('_');
-			txtCedulacliente = new JFormattedTextField(ced);	
-		} catch (Exception e) {}
-		
-		txtCedulacliente.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {									
-				Cliente c=Empresa.getInstance().buscarClientePorCedula(txtCedulacliente.getText());
-				
-				if (c!=null){
-		txtCedulacliente.setText((" ")+c.getId());
-		txtNombre.setText((" ")+c.getNombre());
-		txtApellido.setText((" ")+c.getApellido());
-		txtDireccion.setText((" ")+c.getDireccion());
-		txtTelefonoclient.setText((" ")+c.getTelefono());
-		
-		txtCedulacliente.setEnabled(false);
-		txtNombre.setEnabled(false);
-		txtApellido.setEnabled(false);
-		txtDireccion.setEnabled(false);
-		txtTelefonoclient.setEnabled(false);
-		
-				}
-				else{
-				
-		txtCedulacliente.setText(" ");
-		txtNombre.setText(" ");
-		txtApellido.setText(" ");
-		txtDireccion.setText(" ");
-		txtTelefonoclient.setText(" ");
-		
-		txtCedulacliente.setEnabled(true);
-		txtNombre.setEnabled(true);
-		txtApellido.setEnabled(true);
-		txtDireccion.setEnabled(true);
-		txtTelefonoclient.setEnabled(true);
-		
-				}
-				
-			}
-		});
-		txtCedulacliente.setBounds(76, 32, 128, 20);
-		panelInfo.add(txtCedulacliente);		
-		
-		try {
-			MaskFormatter tel = new MaskFormatter("###-###-####");
-			tel.setPlaceholderCharacter('_');
-			txtTelefonoclient = new JFormattedTextField(tel);
-		} catch (Exception e) {}
-		
-		txtTelefonoclient.setBounds(76, 193, 128, 20);
-		panelInfo.add(txtTelefonoclient);
-		{
-			JLabel lblNewLabel_1 = new JLabel("");
-			lblNewLabel_1.setIcon(new ImageIcon(CrearProyecto.class.getResource("/Imagenes/avatar-grande (3).png")));
-			lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-			lblNewLabel_1.setBounds(458, 26, 217, 104);
-			P2.add(lblNewLabel_1);
-		}
-		
-	
-		
-
-	
-		{
-			P3 = new JPanel();
-			P3.setBackground(Color.WHITE);
-			P3.setBounds(0, 0, 724, 428);
-			contentPanel.add(P3);
-			P3.setLayout(null);
-			P3.setVisible(false);
-			{
-				JPanel panel = new JPanel();
-				panel.setLayout(null);
-				panel.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Informaci\u00F3n del Cliente:", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-				panel.setBackground(Color.WHITE);
-				panel.setBounds(10, 11, 704, 108);
-				P3.add(panel);
-				{
-					JLabel label = new JLabel("Nombre:");
-					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-					label.setBounds(10, 70, 57, 14);
-					panel.add(label);
-				}
-				{
-					txtNombreCmostrado = new JTextField();
-					txtNombreCmostrado.setEditable(false);
-					txtNombreCmostrado.setColumns(10);
-					txtNombreCmostrado.setBounds(77, 67, 408, 21);
-					panel.add(txtNombreCmostrado);
-				}
-				{
-					JLabel label = new JLabel("C\u00E9dula:");
-					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-					label.setBounds(10, 29, 57, 14);
-					panel.add(label);
-				}
-				{
-					txtCedulaMostrada = new JTextField();
-					txtCedulaMostrada.setEditable(false);
-					txtCedulaMostrada.setColumns(10);
-					txtCedulaMostrada.setBounds(77, 24, 117, 21);
-					panel.add(txtCedulaMostrada);
-				}
-				{
-					JLabel label = new JLabel("Tel\u00E9fono:");
-					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-					label.setBounds(204, 29, 57, 14);
-					panel.add(label);
-				}
-				{
-					txtTelMostrado = new JTextField();
-					txtTelMostrado.setEditable(false);
-					txtTelMostrado.setColumns(10);
-					txtTelMostrado.setBounds(268, 24, 217, 21);
-					panel.add(txtTelMostrado);
-				}
-			}
-			{
-				JPanel panel = new JPanel();
-				panel.setLayout(null);
-				panel.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Informaci\u00F3n Del Proyecto:", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-				panel.setBackground(Color.WHITE);
-				panel.setBounds(10, 130, 704, 108);
-				P3.add(panel);
-				{
-					JLabel label = new JLabel("Nombre:");
-					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-					label.setBounds(10, 18, 55, 14);
-					panel.add(label);
-				}
-				{
-					txtNombreProMostrado = new JTextField();
-					txtNombreProMostrado.setEditable(false);
-					txtNombreProMostrado.setColumns(10);
-					txtNombreProMostrado.setBounds(140, 15, 476, 20);
-					panel.add(txtNombreProMostrado);
-				}
-				{
-					JLabel label = new JLabel("Fecha de entrega:");
-					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-					label.setBounds(328, 50, 110, 14);
-					panel.add(label);
-				}
-				{
-					txtFechaEntregaMostrada = new JTextField();
-					txtFechaEntregaMostrada.setText((String) null);
-					txtFechaEntregaMostrada.setEditable(false);
-					txtFechaEntregaMostrada.setColumns(10);
-					txtFechaEntregaMostrada.setBounds(438, 47, 178, 20);
-					panel.add(txtFechaEntregaMostrada);
-				}
-				{
-					JLabel label = new JLabel("Total a pagar:");
-					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-					label.setBounds(10, 81, 85, 14);
-					panel.add(label);
-				}
-				{
-					txtPrecioMostrado = new JTextField();
-					txtPrecioMostrado.setEditable(false);
-					txtPrecioMostrado.setColumns(10);
-					txtPrecioMostrado.setBounds(140, 78, 178, 20);
-					panel.add(txtPrecioMostrado);
-				}
-				{
-					JLabel label = new JLabel("Fecha de Realizaci\u00F3n:");
-					label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 11));
-					label.setBounds(10, 53, 131, 14);
-					panel.add(label);
-				}
-				{
-					txtFechRealizacionMostrada = new JTextField();
-					txtFechRealizacionMostrada.setText((String) null);
-					txtFechRealizacionMostrada.setEditable(false);
-					txtFechRealizacionMostrada.setColumns(10);
-					txtFechRealizacionMostrada.setBounds(140, 50, 178, 20);
-					panel.add(txtFechRealizacionMostrada);
-				}
-			}
-			
-			JPanel panel = new JPanel();
-			panel.setLayout(null);
-			panel.setBorder(new TitledBorder(new LineBorder(new Color(100, 149, 237)), "Equipo de Trabajo", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-			panel.setBackground(Color.WHITE);
-			panel.setBounds(10, 235, 704, 170);
-			P3.add(panel);
-			
-			JScrollPane scrollPane = new JScrollPane();
-			scrollPane.setBounds(10, 22, 684, 137);
-			panel.add(scrollPane);
-		}
 			
 
 	
