fcc801e171bf0b575acf839e88f2a0e7292b3eef
vilmanicoll <vilmanicoll@VilmaNVidal-PC>
2017-12-06 18:40:33 -0400

    Splash hecho

---
+++ FinalProg1/src/Visual/Splash.java
@@ -0,0 +1,50 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class Splash extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Splash frame = new Splash();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Splash() {
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 462, 223);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Splash.class.getResource("/Imagenes/WhatsApp Image 2017-11-30 at 4.19.18 PM.jpeg")));
+		lblNewLabel.setBounds(0,0, 462, 223);
+		contentPane.add(lblNewLabel);
+	}
+}
